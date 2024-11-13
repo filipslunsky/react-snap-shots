09a43d16c23377ac2e54385a614ca9bc6120d3e5
filipslunsky <fslunsky@gmail.com>
2024-11-13 18:04:09 +0100

    changed getData()

--- src/components/Functions.jsx
+++ src/components/Functions.jsx
@@ -1,10 +1,12 @@
-import { createClient } from 'pexels';
-
 const getData = async (query, pageNum) => {
-    const client = createClient('7ibUxLt0ptb15Prf5r0In94ODPWeDrm4KYijPAr0ALdk1PZUv1O751OS');
-    const data = await client.photos.search({ query, page: pageNum, per_page: 30 });
-    const images = data.photos;
-    return images;
-    };
+    const apiKey = '7ibUxLt0ptb15Prf5r0In94ODPWeDrm4KYijPAr0ALdk1PZUv1O751OS';
+    const response = await fetch(`https://api.pexels.com/v1/search?query=${query}&page=${pageNum}&per_page=30`, {
+        headers: {
+            Authorization: apiKey,
+        },
+    });
+    const data = await response.json();
+    return data.photos;
+};
 
 export default getData;
